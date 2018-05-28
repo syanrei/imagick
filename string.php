3598173adf8eff6b600ab1d0cad2c4caea5b8b8b
syanrei <honinbou31751@yahoo.co.jp>
2018-05-28 15:20:16 +0900

    #2 コミット

--- string.php
+++ string.php
@@ -1,9 +1,8 @@
 <?php
-$str=$_GET['str'];
-$file=$_GET['file'];
-
+$str  = $_GET['str'];
+$file = $_GET['file'];
 $image = new Imagick($file);
-
+ 
 $draw = new ImagickDraw();
 $draw->setFont('/usr/share/fonts/ipa-gothic/ipag.ttf');  //フォントファイル
 $draw->setFontSize('24');                    // フォントサイズ
@@ -11,8 +10,6 @@ $draw->setFillColor('#ff0000');              // 文字色
 $draw->annotation(10, 100, $str);
 $image->drawImage($draw);
 
-
-
-header("Content-Type: image/jpeg");
+header("Content-Type: image/png");
 ob_clean();
 echo $image;
